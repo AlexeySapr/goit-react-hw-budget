f983ffc4dd68a872ca53cd33c25acc87d7a80468
Alexey Sapr <alexeysapr@gmail.com>
2022-01-12 10:51:19 +0200

    First Add

--- src/components/header/Header.styled.jsx
+++ src/components/header/Header.styled.jsx
@@ -2,5 +2,5 @@ import styled from '@emotion/styled';
 
 export const Title = styled.h2`
   text-align: center;
-  background-color: ${props => props.theme.colors.primary};
+  background-color: #1976d2;
 `;
